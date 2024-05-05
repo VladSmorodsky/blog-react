a8e0cc1d9f48e0dba1c28eb4f484464dab39c6ff
Vlad Smorodskyi <vsmorodskyi@Vlads-MacBook-Pro.local>
2024-05-05 14:55:11 +0300

    BR-18 Add serch posts by title

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,4 @@
-export const Input = ({label, name, type = 'text', onChange, required = false, value = ''}) => {
+export const Input = ({label, name, type = 'text', onChange, required = false, value = '', className = ''}) => {
     return (
         <>
             {label && (
@@ -8,7 +8,7 @@ export const Input = ({label, name, type = 'text', onChange, required = false, v
                 </label>
             )}
             <input type={type} name={name} id={name}
-                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5"
+                   className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-purple-900 focus:border-purple-900 block w-full p-2.5 ${className}`}
                    placeholder={name.toUpperCase()}
                    value={value}
                    onChange={onChange}
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,7 @@
 import {PostCard} from "../components/PostCard";
 import {useQueryClient} from "@tanstack/react-query";
 import {usePostsQuery} from "../hooks/usePostsQuery";
-import {useState} from "react";
+import {useDeferredValue, useEffect, useState} from "react";
 import Pagination from "rc-pagination";
 import {ChevronLeft} from "../assets/ChevronLeft";
 import {ChevronRight} from "../assets/ChevronRight";
@@ -9,11 +9,16 @@ import {PaginationButton} from "../components/PaginationButton";
 import {Header} from "../components/Header";
 import {useCategoriesQuery} from "../hooks/useCategoriesQuery";
 import {CategoryBadge} from "../components/CategoryBadge";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {Input} from "../components/Input/Input";
+import axios from "axios";
 
 export const BlogPage = () => {
     useQueryClient();
     const [filterParams, setFilterParams] = useState({
         page: 1,
+        search: '',
         category: null
     });
     const {data, isFetching} = usePostsQuery(filterParams);
@@ -35,25 +40,48 @@ export const BlogPage = () => {
         }))
     };
 
+    const setSearch = (value) => {
+        setFilterParams((prevState) => ({
+            ...prevState,
+            page: 1,
+            search: value
+        }))
+    }
+
     return (
         <section>
             <Header />
             <div className="py-20">
                 <div className="lg:px-14 px-6">
                     <div className="text-center">
-                        <span className="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 font-extrabold py-2">Blog</span>
+                        <span
+                            className="text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 font-extrabold py-2">Blog</span>
+                    </div>
+                    <div className='relative flex align-middle my-2'>
+                        <div className='absolute flex self-center left-2 text-purple-900'>
+                            <FontAwesomeIcon icon={faSearch}/>
+                        </div>
+                        <Input name='search' className='px-7 border border-purple-900 sm:text-lg' onChange={(event) => setSearch(event.target.value)} value={filterParams.search}/>
                     </div>
                     <div className={'flex'}>
-                        <CategoryBadge className={'text-xl'} category={defaultCategory} className={`text-xl ${defaultCategory.id === filterParams.category ? 'border border-purple-600' : ''}`} onClick={() => setCategory(defaultCategory.id)}/>
+                        <CategoryBadge category={defaultCategory}
+                                       className={`text-xl ${defaultCategory.id === filterParams.category ? 'border border-purple-600' : ''}`}
+                                       onClick={() => setCategory(defaultCategory.id)}/>
                         {categories?.data.map((category, index) => {
-                            return (<CategoryBadge key={index} className={`text-xl ${category.id === filterParams.category ? 'border border-purple-600' : ''}`} category={category} onClick={() => setCategory(category.id)} />)
+                            return (<CategoryBadge key={index}
+                                                   className={`text-xl ${category.id === filterParams.category ? 'border border-purple-600' : ''}`}
+                                                   category={category} onClick={() => setCategory(category.id)}/>)
                         })}
                     </div>
-                    <div className="mt-9 grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
-                        {data?.data.map((post, index) => {
-                            return <PostCard key={index} post={post}/>;
-                        })}
-                    </div>
+                    {data?.data.length ? (
+                        <div className="mt-9 grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                            {data?.data.map((post, index) => {
+                                return <PostCard key={index} post={post}/>;
+                            })}
+                        </div>
+                    ) : (
+                        <div className='text-center my-2 font-semibold text-lg'>No posts found</div>
+                    )}
                 </div>
             </div>
             {!isFetching && (<Pagination
