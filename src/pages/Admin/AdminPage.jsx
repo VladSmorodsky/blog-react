9421f9cd9d29e206d8b7571981dd5479cbead660
Vlad Smorodskyi <vsmorodskyi@Vlads-MacBook-Pro.local>
2024-05-06 18:02:24 +0300

    BR-20 Add admin responsive: added responsivity to admin posts page

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -10,8 +10,8 @@ export const AdminPage = ({children}) => {
         <Header className={'justify-between'}>
             <Dropdown />
         </Header>
-        <div className={'pt-16 grid lg:grid-cols-[1fr,5fr] md:grid-cols-[1fr,3fr] gap-2 h-lvh overflow-hidden'}>
-            <div className={'md:flex relative flex-col flex-1 pt-0 min-h-0 border-r border-black-500 hidden'}>
+        <div className={'pt-16 grid lg:grid-cols-6 md:grid-cols-8 grid-cols-1 gap-2 h-lvh md:overflow-hidden'}>
+            <div className={'md:grid relative lg:col-span-1 md:col-span-2 pt-0 min-h-0 border-r border-black-500 hidden'}>
                 <div className={'md:flex md:flex-col'}>
                     <MenuItem path={ADMIN_POSTS_PAGE} className='flex'>
                         <PostIcon/>
@@ -23,7 +23,9 @@ export const AdminPage = ({children}) => {
                     </MenuItem>
                 </div>
             </div>
-            {children}
+            <div className='lg:col-span-5 md:col-span-6'>
+                {children}
+            </div>
         </div>
     </>
 }
