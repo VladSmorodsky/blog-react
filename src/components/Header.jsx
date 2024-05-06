e4e7c98c442f81a884deaf872aed2dc3422686d4
Vlad Smorodskyi <vsmorodskyi@Vlads-MacBook-Pro.local>
2024-05-06 15:22:05 +0300

    BR-19 Add dropdown component: - added sign out button - added menu items on small devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import {Logo} from "./Logo";
 
 export const Header = ({children, className}) => {
     return (
-        <div className='bg-[#F7FAFC] fixed h-16 z-10 top-0 w-full border border-b-black-500 lg:px-14 px-6'>
+        <div className='bg-[#F7FAFC] fixed h-16 z-10 top-0 w-full border border-b-black-500 px-2'>
             <div className={`flex items-center h-full ${className}`}>
                 <Link className='flex items-center' to={MAIN_PAGE}>
                     <Logo/>
